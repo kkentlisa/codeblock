a3f731745b9e1a15848671b4c5acdfe719270985
kkentlisa <e.kkenig@gmail.com>
2026-03-01 21:17:13 +0700

    interpreter.js: добавила обработку ошибок

--- scripts/interpreter.js
+++ scripts/interpreter.js
@@ -8,53 +8,77 @@ function GetVariable(name) {
     return variables[name];
 }
 
+function validateOperands(block){
+    if (!block.data.left || !block.data.right) {
+        LogToOutputPanel(`Ошибка в блоке ${block.type}: не все операнды заполнены`);
+        throw new Error(`Не все операнды заполнены`);
+    }
+}
+
+function validateCondition(block){
+    if (!block.data.condition) {
+        LogToOutputPanel(`Ошибка в блоке ${block.type}: не заполнено условие`);
+        throw new Error(`Не заполнено условие`);
+    }
+}
+
 function EvaluateExpression(block) {
     if (block.type === "input") {
         const value = block.data.value;
         if (isNaN(value)) {
-            return GetVariable(value);
+            const variableValue =  GetVariable(value);
+            if (variableValue === undefined){
+                LogToOutputPanel(`Ошибка в блоке ${block.type}: переменная "${value}" не найдена`);
+                throw new Error(`Переменная "${value}" не найдена`);
+            }
+            return variableValue;
         }
         else
             return Number(value);
     }
 
     else if (block.type === "add") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue + rightValue;
     }
 
     else if (block.type === "subtract") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue - rightValue;
     }
 
     else if (block.type === "multiply") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue * rightValue;
     }
 
     else if (block.type === "div") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
 
         if (rightValue === 0) {
-            LogToOutputPanel("Деление на 0!")
-            return 0;
+            LogToOutputPanel(`Ошибка в блоке ${block.type}: деление на 0`);
+            throw new Error(`Деление на 0`);
         }
 
         return Math.floor(leftValue / rightValue);
     }
 
     else if (block.type === "mod") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
 
         if (rightValue === 0) {
-            LogToOutputPanel("Деление на 0!")
-            return 0;
+            LogToOutputPanel(`Ошибка в блоке ${block.type}: деление на 0`);
+            throw new Error(`Деление на 0`);
         }
 
         return leftValue % rightValue;
@@ -63,54 +87,66 @@ function EvaluateExpression(block) {
 
 function EvaluateCondition(block) {
     if (block.type === "gt") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue > rightValue;
     }
 
     else if (block.type === "lt") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue < rightValue;
     }
 
     else if (block.type === "eq") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue === rightValue;
     }
 
     else if (block.type === "neq") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue !== rightValue;
     }
 
     else if (block.type === "gte") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue >= rightValue;
     }
 
     else if (block.type === "lte") {
+        validateOperands(block);
         const leftValue = EvaluateExpression(block.data.left);
         const rightValue = EvaluateExpression(block.data.right);
         return leftValue <= rightValue;
     }
 
     else if (block.type === "and") {
+        validateOperands(block);
         const leftValue = EvaluateCondition(block.data.left);
         const rightValue = EvaluateCondition(block.data.right);
         return leftValue && rightValue;
     }
 
     else if (block.type === "or") {
+        validateOperands(block);
         const leftValue = EvaluateCondition(block.data.left);
         const rightValue = EvaluateCondition(block.data.right);
         return leftValue || rightValue;
     }
 
     else if (block.type === "not") {
+        if (!block.data.operand) {
+            LogToOutputPanel(`Ошибка в блоке ${block.type}: операнд не заполнен`);
+            throw new Error(`Операнд не заполнен`);
+        }
         const operandValue = EvaluateCondition(block.data.operand);
         return !operandValue;
     }
@@ -124,10 +160,15 @@ function ExecuteBlock(block) {
             SetVariable(block.data.name, block.data.value);
             break;
         case "assignValue":
+            if (!block.data.value) {
+                LogToOutputPanel(`Ошибка в блоке ${block.type}: не заполнено значение для присваивания`);
+                throw new Error(`Не заполнено значение для присваивания`);
+            }
             const value = EvaluateExpression(block.data.value);
             SetVariable(block.data.variable, value);
             break;
         case "if":
+            validateCondition(block);
             if (EvaluateCondition(block.data.condition)) {
                 block.data.thenBlocks.forEach(id =>{
                     const childBlock = GetBlockById(id);
@@ -136,6 +177,7 @@ function ExecuteBlock(block) {
             }
             break;
         case "ifElse":
+            validateCondition(block);
             if (EvaluateCondition(block.data.condition)) {
                 block.data.thenBlocks.forEach(id =>{
                     const childBlock = GetBlockById(id);
@@ -150,6 +192,7 @@ function ExecuteBlock(block) {
             }
             break;
         case "while":
+            validateCondition(block);
             while (EvaluateCondition(block.data.condition)){
                 block.data.bodyBlocks.forEach(id =>{
                     const childBlock = GetBlockById(id);
@@ -159,7 +202,13 @@ function ExecuteBlock(block) {
             break;
         case "print": {
             const value = GetVariable(block.data.variable);
-            LogToOutputPanel(String(value));
+            if (value === undefined){
+                LogToOutputPanel(`Ошибка в блоке ${block.type}: переменная "${block.data.variable}" не найдена`);
+                throw new Error(`Переменная "${block.data.variable}" не найдена`);
+            }
+            else {
+                LogToOutputPanel(String(value));
+            }
             break;
         }
 
