9d5e048969903d735a2b7fa740e50d1e5e70fba3
ekaterinakondrasova <kate_kondrashova07@mail.ru>
2026-03-03 21:26:57 +0700

    добавила функцию rectsIntersect для определения пересечения блоков

--- scripts/BlockNesting.js
+++ scripts/BlockNesting.js
@@ -16,3 +16,9 @@ function connectToSlot(parentId, childId, slotName) {
     renderAllBlocks(blocksInWorkSpace);
 }
 
+function rectsIntersect(rect1, rect2){
+    return !(rect2.left > rect1.right ||
+        rect2.right < rect1.left ||
+        rect2.top > rect1.bottom ||
+        rect2.bottom < rect1.top);
+}
